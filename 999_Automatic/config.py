9157a4b944de096b47b41933c99da25885c55986
jjviton <j3viton@gmail.com>
2023-06-02 18:36:52 +0200

    mejoras Alpaca

--- 999_Automatic/config.py
+++ 999_Automatic/config.py
@@ -5,6 +5,7 @@ Created on Wed Apr  5 11:31:34 2023
 @author: INNOVACION
 """
 
-API_KEY = "PKSYRUMFRFG72QD0R3LA"
-SECRET_KEY= "Fet7mgyu8rGHhdWhxTd5rcpJbGp6zULkCNGuPI4e"
+
+API_KEY = "PK1O23TDVEIQ474JYW18"
+SECRET_KEY= "2kcqKnpyGtd67wk10b2udJZSh96QbxbGhZfQ2xOZ"
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets" # En caso de usar la versión de prueba de Alpaca
